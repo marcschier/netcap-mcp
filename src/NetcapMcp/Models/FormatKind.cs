d82974126444cfa3e0b8a739f608e3fc2af7aad4
marcschier <marcschier@users.noreply.github.com>
2026-06-04 12:28:10 +0200

    Remove Microsoft copyright headers; use Apache-2.0 SPDX-style header

--- src/NetcapMcp/Models/FormatKind.cs
+++ src/NetcapMcp/Models/FormatKind.cs
@@ -1,7 +1,4 @@
-// ------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation.  All rights reserved.
-//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0.
 
 namespace NetcapMcp.Models;
 
